b78276d744eaafc114bde586c37983f1a0b79ed3
Leishiguang <shiguang_lei@126.com>
2017-04-06 00:41:18 -0700

    done myblog_test

---
+++ Rails_Guides/blog/db/migrate/20170406065400_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :comments do |t|
+      t.string :comment
+      t.string :commenter
+      t.text :body
+      t.references :article, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
