b02904e212bae7caa824bce42cf775c3948e7cbc
Leishiguang <shiguang_lei@126.com>
2017-05-21 18:20:25 -0700

    add some example

--- Ruby_test/Q13.rb
+++ Ruby_test/Q13.rb
@@ -34,7 +34,7 @@ end
 print "\n"
 =end
 
-puts "chapters 13 Question 3."
+puts "Ary3 in chapters 13 Question 3."
 ary3 = Array.new()
 ary1.each do |elem|
   if (elem % 3) == 0
@@ -47,6 +47,64 @@ ary3.each do |elem|
 end
 print "\n"
 
+puts "Ary1 in chapters 13 Question 3."
+
 =begin
-ary1.each_with_index()
+ary1.each_with_index do |elem, i|
+  if (elem % 3) != 0
+    ary1.delete_at(i)
+  end
+end
 =end
+
+ary1.delete_if{|elem| elem % 3 != 0}
+
+ary1.each do |elem|
+  print elem,"\t"
+end
+print "\n"
+
+
+puts "chapters 13 Question 4."
+
+ary1.reverse!
+ary1.each do |elem|
+  print elem,"\t"
+end
+print "\n"
+
+puts "chapters 13 Question 5."
+count = 0
+ary1.each do |elem|
+  count += elem
+end
+puts "The sum of ary1 is #{count}"
+
+
+puts "chapters 13 Question 6."
+ary6 = Array.new(100) do |i|
+  i + 1
+end
+result = Array.new()
+10.times do |i|
+  result << ary6[i*10..i*10+9]
+end
+
+result.each do |elem|
+  print elem
+end
+
+puts "chapters 13 Question 7."
+def sum_array(ary1, ary2)
+  i = 0
+  result = []
+  while i < ary1.length
+    result << ary1[i] + ary2[i]
+    i += 1
+  end
+  return result
+end
+
+num1 = [1,2,3]
+num2 = [4,6,8]
+p sum_array(num1, num2)
