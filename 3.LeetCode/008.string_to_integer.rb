3a71e1df33246afea6e8769b647611abc52051da
leishiguang <lx6ting@163.com>
2017-06-30 10:51:44 +0800

    solved

--- 3.LeetCode/008.string_to_integer.rb
+++ 3.LeetCode/008.string_to_integer.rb
@@ -1,12 +1,35 @@
+# @param {String} str
+# @return {Integer}
+=begin
+def my_atoi(str)
+  i = str.to_i
+  if i > 2**31 - 1
+    return 2**31 - 1
+  end
+  if i < -2**31
+    return -2**31
+  end
+  return i
+end
+=end
 
 # @param {String} str
 # @return {Integer}
 def my_atoi(str)
-  str.delete! " "
+  #str.delete! " "
+  i = 0
+  str.each_char do |c|
+    if c == " "
+        str[i] = "0"
+    else
+        break
+    end
+    i += 1
+  end
   key = true
-  if str[0] == "-" or str[0] == "+"
-    key = false if str[0] == "-"
-    str[0] = "0"
+  if str[i] == "-" or str[i] == "+"
+    key = false if str[i] == "-"
+    str[i] = "0"
   end
   result, i = "", 0
   str.each_char do |c|
