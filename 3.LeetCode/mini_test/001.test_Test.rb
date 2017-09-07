08adfc0b84d373284dcf99064b8c892daa6f2aab
leishiguang <lx6ting@163.com>
2017-09-07 11:24:58 +0800

    test MiniTest

--- 3.LeetCode/mini_test/001.test_Test.rb
+++ 3.LeetCode/mini_test/001.test_Test.rb
@@ -1,17 +1,18 @@
 require 'minitest/autorun'
-require File.dirname(__FILE__) + '/../000.Test'
+    require File.dirname(__FILE__) + '/../000.Test'
+
+    class TestPeople < Minitest::Test
+      def setup
+        @people = People.new
+      end
+
+      def test_name_is_ting
+        assert_equal "ting", @people.name
+      end
+
+      def test_name_is_ting_2
+        assert_equal "ting", @people.name
+      end
+    end
 
 
-class TestPeople < Minitest::Test
-  def setup
-    @people = People.new
-  end
-
-  def test_name_is_ting
-    assert_equal "ting", @people.name
-  end
-
-  def test_name_is_ting_2
-    assert_equal "ting", @people.name
-  end
-end
---
+++ 3.LeetCode/mini_test/053. test_maximum_subarray.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require File.dirname(__FILE__) + '/../053.maximun_subarray'
+class Test_053 < Minitest::Test
+
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+
+  end
+
+  def test_1
+    assert_equal 6, max_sub_array([-2,1,-3,4,-1,2,1,-5,4])
+  end
+
+  def test_2
+    assert_equal [1], max_sub_array([2])
+  end
+
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
+
+  def teardown
+    # Do nothing
+  end
+
+end
